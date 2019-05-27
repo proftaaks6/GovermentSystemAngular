3c4165573835e8db8e465dac37f2c6d67233fde0
Wouter <woutervanacht@hotmail.com>
2019-05-27 20:04:59 +0200

    Moved directive to new module

---
+++ src/app/components/helpers/helpers.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NumberOnlyDirective } from 'src/app/models/number.directive';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    NumberOnlyDirective
+  ]
+})
+export class HelpersModule { }
