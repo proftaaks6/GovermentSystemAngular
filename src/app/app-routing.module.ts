f08a6c62dc1ac29319074011d756d913374e7022
Wouter <woutervanacht@hotmail.com>
2019-05-29 11:45:14 +0200

    Fixed tracker naming

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,16 +7,11 @@ import { AuthGuard } from './shared/guard/auth.guard';
 
 const routes: Routes = [
   { path: '', component: HomeComponent, canActivate: [AuthGuard] },
-  { path: 'cartracker', loadChildren: './components/cartracker/cartracker.module#CartrackerModule', canActivate: [AuthGuard] },
+  { path: 'tracker', loadChildren: './components/cartracker/cartracker.module#CartrackerModule', canActivate: [AuthGuard] },
   { path: 'user', loadChildren: './components/user/user.module#UserModule', canActivate: [AuthGuard] },
   {path: 'vehicle', loadChildren: './components/vehicle/vehicle.module#VehicleModule', canActivate: [AuthGuard] },
   {path: 'naw', loadChildren: './components/nawinformation/nawinformation.module#NawinformationModule', canActivate: [AuthGuard] },
   {path: 'regionselect', loadChildren: './components/regionselect/regionselect.module#RegionselectModule', canActivate: [AuthGuard] },
-  { path: '', component: HomeComponent, canActivate: [AuthGuard] },
-  { path: 'cartracker', loadChildren: './components/cartracker/cartracker.module#CartrackerModule', canActivate: [AuthGuard] },
-  { path: 'user', loadChildren: './components/user/user.module#UserModule', canActivate: [AuthGuard] },
-  { path: 'vehicle', loadChildren: './components/vehicle/vehicle.module#VehicleModule', canActivate: [AuthGuard] },
-  { path: 'naw', loadChildren: './components/nawinformation/nawinformation.module#NawinformationModule', canActivate: [AuthGuard] },
   { path: 'login', component: LoginComponent }
 ];
 
